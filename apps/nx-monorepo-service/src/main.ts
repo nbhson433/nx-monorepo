0f77f3feab9318a1f3138c32d04fd3c70a766d26
Son Nguyen <nbhson43@gmail.com>
2022-05-28 21:11:57 +0700

    Add nodeJS

--- apps/nx-monorepo-service/src/main.ts
+++ apps/nx-monorepo-service/src/main.ts
@@ -1,22 +1,46 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
+// Default
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-
 import { AppModule } from './app/app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
-}
+// Express
+import { Todo } from '@nx-monorepo/data';
+import * as express from 'express';
 
-bootstrap();
+// Default
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   const globalPrefix = 'api';
+//   app.setGlobalPrefix(globalPrefix);
+//   const port = process.env.PORT || 3333;
+//   await app.listen(port);
+//   Logger.log(
+//     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+//   );
+// }
+// bootstrap();
+
+
+// Express
+const app = express();
+const todos: Todo[] = [
+  {
+    title: 'Login page is broken'
+  },
+  {
+    title: 'Everything is broken'
+  }
+];
+app.get('/api/todos', (req, res) => {
+  res.send(JSON.stringify(todos));
+});
+app.post('/api/addTodo', (req, res) => {
+  todos.push({
+    title: `New todo ${Math.floor(Math.random() * 1000)}`,
+  });
+  res.send(JSON.stringify(todos));
+})
+const port = 3333;
+app.listen(port, () => {
+  console.log(`Listening at http://localhost:${port}`);
+});
